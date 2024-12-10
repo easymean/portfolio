9ea562da503d3b4fa5f227f3f4ba2ab5a8c67636
easymean <m2162003@naver.com>
2024-12-10 09:34:22 +0900

    style update

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import './App.css';
-import { ScrollContainer } from './components/layout/one-page-scroll';
-import { Page } from './components/layout/one-page-scroll/Page';
-import { TITLE } from './consts';
-import { About } from './pages/about';
-
+import { Infro } from './pages/Intro';
 import { Projects } from './pages/projects';
 import { Skills } from './pages/skills';
 
 function App() {
   return (
-    <>
-      <div className="one-page-scroll">
-        <ScrollContainer>
-          <Page id={TITLE.ABOUT}>
-            <About />
-          </Page>
-          <Page id={TITLE.SKILLS}>
-            <Skills />
-          </Page>
-          <Page id={TITLE.PROJECTS}>
-            <Projects />
-          </Page>
-        </ScrollContainer>
-      </div>
-    </>
+    <div className="app">
+      <Infro />
+      <Skills />
+      <Projects />
+    </div>
   );
 }
 
---
+++ src/pages/Intro/index.tsx
@@ -0,0 +1,10 @@
+import './style.scss';
+
+export const Infro = () => {
+  return (
+    <div className="about">
+      <div className="title slide-left">Frontend Developer</div>
+      <div className="title slide-right">LEE JIMIN</div>
+    </div>
+  );
+};
