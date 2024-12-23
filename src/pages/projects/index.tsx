6b7e65b923b321c5fe564f27203f608a0a251837
easymean <m2162003@naver.com>
2024-12-23 22:54:46 +0900

    카드 뒤집기 효과

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,9 +1,80 @@
+import { Slider } from '@/components/slider';
+import { CardSlider } from './CardSlider';
 import './style.scss';
+import { useState } from 'react';
 
 export const Projects = () => {
+  const [selectedId, setSelectedId] = useState<string>('varco');
+  const middleSlider = [
+    {
+      id: 'varco',
+      content: <div className="slide-content-wrapper">varco</div>,
+    },
+    {
+      id: 'speech',
+      content: <div className="slide-content-wrapper">speech</div>,
+    },
+    {
+      id: 'miniverse',
+      content: <div className="slide-content-wrapper">miniverse</div>,
+    },
+    { id: 'bard', content: <div className="slide-content-wrapper">bard</div> },
+  ];
+  const cards = [
+    {
+      groupId: 'varco',
+      id: 'varco art',
+      front: { content: <>front</> },
+      back: { content: <>back</> },
+    },
+    {
+      groupId: 'varco',
+      id: 'varco ui',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'varco',
+      id: 'varco text',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'varco',
+      id: 'design system',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'varco',
+      id: 'speech',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'miniverse',
+      id: 'miniverse',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'bard',
+      id: 'bard',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+  ];
   return (
     <div className="projects">
-      <div>프로젝트 소개</div>
+      <div className="projects-wrapper">
+        <div className="title">PROJECTS</div>
+        <Slider
+          items={middleSlider}
+          colWidth={'100%'}
+          selectedId={selectedId}
+        />
+        <CardSlider items={cards} />
+      </div>
     </div>
   );
 };
