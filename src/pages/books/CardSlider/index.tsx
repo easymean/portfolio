0633974f3980667c5a85fe6589410d6c46419a5e
easymean <m2162003@naver.com>
2024-12-25 14:56:27 +0900

    book에 card slider 적용

---
+++ src/pages/books/CardSlider/index.tsx
@@ -0,0 +1,39 @@
+import './style.scss';
+import React from 'react';
+
+type Props = {
+  items: ({
+    groupId: string;
+  } & CardProps)[];
+};
+export const CardSlider = React.forwardRef<HTMLUListElement, Props>(
+  ({ items = [] }, ref) => {
+    return (
+      <div className="card-slider">
+        <ul className="card-list" ref={ref}>
+          {items.map((el) => (
+            <Card key={el.groupId + el.id} {...el} />
+          ))}
+        </ul>
+      </div>
+    );
+  },
+);
+
+type CardProps = {
+  id: string;
+  front: {
+    content: React.ReactNode;
+  };
+  back: {
+    content: React.ReactNode;
+  };
+};
+const Card = ({ front, back }: CardProps) => {
+  return (
+    <li className="card">
+      <div className="card-container front">{front.content}</div>
+      <div className="card-container back">{back.content}</div>
+    </li>
+  );
+};
---
+++ src/pages/books/index.tsx
@@ -0,0 +1,111 @@
+import { Slider } from '@/components/slider';
+import { CardSlider } from './CardSlider';
+import './style.scss';
+import { useEffect, useRef, useState } from 'react';
+
+export const Books = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLUListElement>(null);
+
+  const [selectedId, setSelectedId] = useState<string>('varco');
+
+  const middleSlider = [
+    {
+      id: 'varco',
+      content: <div className="slide-content-wrapper">varco</div>,
+    },
+    {
+      id: 'speech',
+      content: <div className="slide-content-wrapper">speech</div>,
+    },
+    {
+      id: 'miniverse',
+      content: <div className="slide-content-wrapper">miniverse</div>,
+    },
+    { id: 'bard', content: <div className="slide-content-wrapper">bard</div> },
+  ];
+  const cards = [
+    {
+      groupId: 'varco',
+      id: 'varco art',
+      front: { content: <>front</> },
+      back: { content: <>back</> },
+    },
+    {
+      groupId: 'varco',
+      id: 'varco ui',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'varco',
+      id: 'varco text',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'varco',
+      id: 'design system',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'varco',
+      id: 'speech',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+    {
+      groupId: 'miniverse',
+      id: 'miniverse',
+      front: { content: <></> },
+      back: { content: <></> },
+    },
+  ];
+
+  const observer = useRef<IntersectionObserver>();
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!scrollRef.current || !containerRef.current) {
+        return;
+      }
+      const scrollOffset = window.scrollY - containerRef.current.offsetTop;
+      const translateX = Math.max(0, scrollOffset);
+      scrollRef.current.style.transform = `translateX(-${translateX}px)`;
+    };
+
+    observer.current = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          window.addEventListener('scroll', handleScroll);
+        } else {
+          window.removeEventListener('scroll', handleScroll);
+        }
+      },
+      { threshold: 0.2 },
+    );
+
+    if (containerRef.current) {
+      observer.current?.observe(containerRef.current);
+    }
+
+    return () => {
+      observer.current?.disconnect();
+    };
+  }, []);
+  return (
+    <div className="books" ref={containerRef}>
+      <div className="books-wrapper">
+        <div className="title">BOOKS</div>
+        <Slider
+          items={middleSlider}
+          colWidth={'100%'}
+          selectedId={selectedId}
+        />
+        <div className="scroll-container">
+          <CardSlider items={cards} ref={scrollRef} />
+        </div>
+      </div>
+    </div>
+  );
+};
