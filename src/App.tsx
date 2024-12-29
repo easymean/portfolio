243da82a7d51f8ee7be6c27d8470303b8f6d0bd7
easymean <m2162003@naver.com>
2024-12-29 16:27:48 +0900

    헤더에 theme 전환 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css';
 import { Infro } from '@/pages/Intro';
 import { Projects } from '@/pages/projects';
 import { Skills } from '@/pages/skills';
+import { Books } from './pages/books';
+import { StickyHeader } from './components/layout/sticky-header';
 
 function App() {
   return (
     <div className="app">
+      <StickyHeader />
       <Infro />
       <Skills />
+      <Books />
       <Projects />
     </div>
   );
--- src/components/layout/sticky-header/index.tsx
+++ src/components/layout/sticky-header/index.tsx
@@ -1,8 +1,36 @@
+import { getTheme, THEME } from '@/consts';
+import { useEffect, useState } from 'react';
+import './style.scss';
+
 export const StickyHeader = () => {
+  const [theme, setTheme] = useState('');
+
+  const setLocalStorageTheme = (theme: string) => {
+    const themeAsEnum = getTheme(theme);
+    setTheme(themeAsEnum);
+    localStorage.setItem('theme', themeAsEnum);
+    const app = document.body.querySelector('.app') as HTMLElement;
+    if (app) {
+      console.log(app);
+      app.style.colorScheme = themeAsEnum;
+    }
+  };
+
+  const toggleTheme = () => {
+    const nextTheme = theme === THEME.DARK ? THEME.LIGHT : THEME.DARK;
+    setLocalStorageTheme(nextTheme);
+  };
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme') ?? THEME.DARK;
+    setLocalStorageTheme(theme);
+  }, []);
+
   return (
     <nav className="sticky-header">
-      <button></button>
-      <button></button>
+      <button className="theme-btn" onClick={toggleTheme}>
+        {theme === THEME.DARK ? 'DARK' : 'LIGHT'}
+      </button>
     </nav>
   );
 };
