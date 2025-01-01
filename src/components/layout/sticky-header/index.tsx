08caf80e781ed249d0dc62ef153f9583f8ef5c1d
easymean <m2162003@naver.com>
2025-01-02 00:58:57 +0900

    헤더 수정

--- src/components/layout/sticky-header/index.tsx
+++ src/components/layout/sticky-header/index.tsx
@@ -6,6 +6,14 @@ export const StickyHeader = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <nav className="sticky-header">
+      <div className="link-list">
+        <a href="https://github.com/easymean" target="_blank">
+          GITHUB
+        </a>
+        <a href="https://velog.io/@easymean/posts" target="_blank">
+          블로그
+        </a>
+      </div>
       <button className="theme-btn" onClick={toggleTheme}>
         {theme === THEME.DARK ? 'DARK' : 'LIGHT'}
       </button>
