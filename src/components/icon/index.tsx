febeb8345ba94fa652fcaa0a29f784c9420d7c67
easymean <m2162003@naver.com>
2025-01-11 22:42:21 +0900

    icon 추가

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,27 @@
+import ts from '@/assets/svg/icon-ts.svg?react';
+import js from '@/assets/svg/icon-js.svg?react';
+import html from '@/assets/svg/icon-html.svg?react';
+import css from '@/assets/svg/icon-css.svg?react';
+import sass from '@/assets/svg/icon-sass.svg?react';
+import react from '@/assets/svg/icon-react.svg?react';
+import vue from '@/assets/svg/icon-vue.svg?react';
+import mariadb from '@/assets/svg/icon-mariadb.svg?react';
+import psql from '@/assets/svg/icon-psql.svg?react';
+import nestjs from '@/assets/svg/icon-nestjs.svg?react';
+import spring from '@/assets/svg/icon-spring.svg?react';
+
+const Icon = {
+  ts: ts,
+  js: js,
+  html: html,
+  css: css,
+  sass: sass,
+  react: react,
+  vue: vue,
+  mariadb: mariadb,
+  psql: psql,
+  nestjs: nestjs,
+  spring: spring,
+};
+
+export default Icon;
