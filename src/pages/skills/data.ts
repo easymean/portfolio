7e766ea880ec3dfd0c26856de94b5cc71b109ebd
easymean <m2162003@naver.com>
2025-01-12 17:53:19 +0900

    constant data 분리

---
+++ src/pages/skills/data.ts
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 'fe',
+    label: 'FRONTEND',
+    content:
+      'React와 Redux, Zustand의 전역 상태 관리 라이브러리를 사용하여 복잡한 상태를 관리할 수 있고 Redux-saga, Redux-thunk, React-Query를 사용해본 경험이 있습니다.\nVue3, Composition API, Pinia를 사용하여 어플리케이션을 개발한 경험이 있습니다.',
+  },
+  {
+    id: 'be',
+    label: 'BACKEND',
+    content: `Spring Boot와 NestJS + TypeORM을 사용한 경험이 있습니다.\nRESTful API를 설계하고 구현할 수 있습니다.\n검색을 통해 CRUD를 사용한 간단한 기능과 및 로그인 기능 구현이 가능합니다.`,
+  },
+  {
+    id: 'db',
+    label: 'DATABASE',
+    content: `서버를 설치하고 환경을 구성한 경험이 있습니다.\nGUI 도구를 활용하여 DB와 연결하고 SQL 쿼리를 작성할 수 있습니다.\n간단한 데이터 스키마를 설계할 수 있습니다.`,
+  },
+];
--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -3,53 +3,15 @@ import { Carousel } from '@/components/carousel';
 import './style.scss';
 import { useEffect, useRef, useState } from 'react';
 import Icon from '@/components/icon';
+import { data } from './data';
 
 export const Skills = () => {
   const [selectedIdx, setSelectedIdx] = useState(0);
 
-  const items = [
-    {
-      id: 'fe',
-      label: 'FRONTEND',
-      content: (
-        <div className="tab-panel-wrapper">
-          React와 Redux, Zustand의 전역 상태 관리 라이브러리를 사용하여 복잡한
-          상태를 관리할 수 있고 Redux-saga, Redux-thunk, React-Query를 사용해본
-          경험이 있습니다.
-          <br />
-          Vue3, Composition API, Pinia를 사용하여 어플리케이션을 개발한 경험이
-          있습니다.
-        </div>
-      ),
-    },
-    {
-      id: 'be',
-      label: 'BACKEND',
-      content: (
-        <div className="tab-panel-wrapper">
-          Spring Boot와 NestJS + TypeORM을 사용한 경험이 있습니다.
-          <br />
-          RESTful API를 설계하고 구현할 수 있습니다.
-          <br />
-          검색을 통해 CRUD를 사용한 간단한 기능과 및 로그인 기능 구현이
-          가능합니다.
-        </div>
-      ),
-    },
-    {
-      id: 'db',
-      label: 'DATABASE',
-      content: (
-        <div className="tab-panel-wrapper">
-          서버를 설치하고 환경을 구성한 경험이 있습니다.
-          <br />
-          GUI 도구를 활용하여 DB와 연결하고 SQL 쿼리를 작성할 수 있습니다.
-          <br />
-          간단한 데이터 스키마를 설계할 수 있습니다.
-        </div>
-      ),
-    },
-  ];
+  const items = data.map((el) => ({
+    ...el,
+    content: <div className="tab-panel-wrapper">{el.content}</div>,
+  }));
 
   const sliders = [
     {
@@ -67,7 +29,7 @@ export const Skills = () => {
   ];
 
   const onClickTab = (id: string) => {
-    const findIdx = items.findIndex((item) => item.id === id);
+    const findIdx = data.findIndex((item) => item.id === id);
     setSelectedIdx(findIdx);
   };
 
