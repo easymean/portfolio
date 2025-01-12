adb0c1749c94168a858ebf6ac64dda9cd2f346f6
easymean <m2162003@naver.com>
2025-01-12 17:59:48 +0900

    스타일 수정

--- src/pages/books/mobile/index.tsx
+++ src/pages/books/mobile/index.tsx
@@ -5,13 +5,12 @@ import { Carousel } from '@/components/carousel';
 import Icon from '@/components/icon';
 
 export const BooksMobile = () => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const cards = [
-    ...data.map((el) => ({ id: el.id, content: <Card {...el} imgSrc="" /> })),
-  ];
-
   const [selectedIdx, setSelectedIdx] = useState(0);
 
+  const cards = data.map((el) => ({
+    id: el.id,
+    content: <Card {...el} imgSrc="" />,
+  }));
   const onClickLeft = useCallback((curIdx: number) => {
     const nextIdx = Math.max(0, curIdx - 1);
     setSelectedIdx(nextIdx);
@@ -31,7 +30,7 @@ export const BooksMobile = () => {
 
   return (
     <div className="books-mobile">
-      <div className="books-wrapper">
+      <div className="books-mobile-wrapper">
         <div className="title">BOOKS</div>
         <div className="carousel-container">
           <Carousel
